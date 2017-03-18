d8dbc73826d12cc49a7d076c82ca4a335b6bbaea
sasaco <sasaco@live.jp>
2017-03-18 23:57:08 +0900

    ngHandsontable 導入

--- RCNonlinearServer/Script/page2.js
+++ RCNonlinearServer/Script/page2.js
@@ -1,7 +1,16 @@
 ﻿
 // コンテンツ関連のスクリプト -------------------------
 app.controller('page2Controller', function ($scope) {
-
+    $scope.data = [
+        {
+            'name': 'Bob',
+            'email': 'bob@sample.com'
+        },
+        {
+            'name': 'John',
+            'email': 'john@sample.com'
+        }
+    ];
 });
 
 
