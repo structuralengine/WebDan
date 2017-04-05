7c674d6b78ed1b5cf2d848f99d9acd9ec759493b
sasaco <sasaco@live.jp>
2017-04-05 09:48:40 +0900

    いろいろ修正

--- docs/js/controllers/page2/Page2.controller.js
+++ docs/js/controllers/page2/Page2.controller.js
@@ -4,14 +4,76 @@ app.controller('Page2Controller', ['$scope', function ($scope) {
 
     var ctrl = this;
 
-     ctrl.data = [
-        {
-            'name': 'Bob',
-            'email': 'bob@sample.com'
-        },
-        {
-            'name': 'John',
-            'email': 'john@sample.com'
-        }
-     ];
+
+    ctrl.data = [
+      {
+          "id": 1,
+          "name": {
+              "first": "John",
+              "last": "Schmidt"
+          },
+          "address": "45024 France",
+          "price": 760.41,
+          "isActive": "Yes",
+          "product": {
+              "description": "Fried Potatoes",
+              "options": [
+                {
+                    "description": "Fried Potatoes",
+                    "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png"
+                },
+                {
+                    "description": "Fried Onions",
+                    "image": "//a248.e.akamai.net/assets.github.com/images/icons/emoji/fries.png"
+                }
+              ]
+          }
+      },
+      {
+          "id": 2,
+          "name": {
+              "first": "Tead",
+              "last": "Fancy"
+          },
+          "address": "94115 Japan",
+          "price": 982.32,
+          "isActive": "No",
+          "product": {
+              "description": "Felis silvestris catus",
+              "options": [
+                {
+                    "description": "Felis silvestris catus",
+                    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg"
+                },
+                {
+                    "description": "Felis silvestris lybica",
+                    "image": "https://upload.wikimedia.org/wikipedia/commons/a/ae/AfricanWildCat.jpg?download"
+                }
+              ]
+          }
+      },
+      {
+          "id": 3,
+          "name": {
+              "first": "Tead",
+              "last": "Fancy"
+          },
+          "address": "94115 Ronson",
+          "price": 982.32,
+          "isActive": "No",
+          "product": {
+              "description": "Felis silvestris catus",
+              "options": [
+                {
+                    "description": "Felis silvestris catus",
+                    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/1024px-Cat_poster_1.jpg"
+                },
+                {
+                    "description": "Felis silvestris lybica",
+                    "image": "https://upload.wikimedia.org/wikipedia/commons/a/ae/AfricanWildCat.jpg?download"
+                }
+              ]
+          }
+      }
+    ];
 }]);
