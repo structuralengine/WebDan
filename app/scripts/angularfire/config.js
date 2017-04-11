f07c44578f5a834920133a42b6c398731c56d6af
a311cb <tokunaga+a311cb@tokunaga-is.com>
2017-04-11 16:15:00 +0900

    sasaco さんに渡すため、一度 build する。

--- app/scripts/angularfire/config.js
+++ app/scripts/angularfire/config.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDeEHtB9db5EQUI9Dp9cR9fMvPSVjW-U4s",
-  authDomain: "test-ca1f8.firebaseapp.com",
-  databaseURL: "https://test-ca1f8.firebaseio.com",
-  projectId: "test-ca1f8",
-  storageBucket: "test-ca1f8.appspot.com",
-  messagingSenderId: "230339306250"
+  apiKey: 'AIzaSyDeEHtB9db5EQUI9Dp9cR9fMvPSVjW-U4s',
+  authDomain: 'test-ca1f8.firebaseapp.com',
+  databaseURL: 'https://test-ca1f8.firebaseio.com',
+  projectId: 'test-ca1f8',
+  storageBucket: 'test-ca1f8.appspot.com',
+  messagingSenderId: '230339306250'
 };
 firebase.initializeApp(config);
 
