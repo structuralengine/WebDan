3c016cf44bea71ed4ed5e82e7f64d4969a68d4d7
a311cb <tokunaga+a311cb@tokunaga-is.com>
2017-05-18 22:38:23 +0900

    file-saver.js によるファイルのダウンロード。

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -8,13 +8,22 @@
  * Controller of the webdan
  */
 angular.module('webdan')
-  .controller('MenuCtrl', ['$scope', '$log', 'tmp',
-    function ($scope, $log, tmp) {
+  .controller('MenuCtrl', ['$scope', '$log', 'tmp', 'moment',
+    function ($scope, $log, tmp, moment) {
       let menu = this;
 
       menu.createNewBucket = function() {
         tmp.data = {};
         $log.debug(tmp);
       };
+
+      menu.saveAsFile = function() {
+        let content = angular.toJson(tmp.data);
+        let timestamp = moment().format('YYYYMMDD-HHmmss');
+        let filename = "webdan."+ timestamp +".json";
+        let type = {type: "application/json; charset=utf-8"};
+        let file = new File([content], filename, type);
+        saveAs(file);
+      }
     }
   ]);
