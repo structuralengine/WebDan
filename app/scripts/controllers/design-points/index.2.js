420ca2a682f01923964b0c94d383ef6bd448810d
a311cb <tokunaga+a311cb@tokunaga-is.com>
2017-05-22 16:54:59 +0900

    sample2.dsd 型「算出点」画面に、「部材番号」を追加。

--- app/scripts/controllers/design-points/index.2.js
+++ app/scripts/controllers/design-points/index.2.js
@@ -12,14 +12,24 @@ angular.module('webdan')
     function ($scope, $log, DesignPoint, Group) {
       let ctrl = this;
 
-      ctrl.settings = {
-        rowHeaders: true,
-        colHeaders: true,
-        nestedHeaders: DesignPoint.nestedHeaders,
-        columns: DesignPoint.columns
-      };
-
       function init() {
+        let nestedHeaders = angular.copy(DesignPoint.nestedHeaders);
+        nestedHeaders[0].unshift('部材番号');
+        nestedHeaders[1].unshift('');
+
+        let columns = angular.copy(DesignPoint.columns);
+        columns.unshift({
+          data: 'Member.m_no',
+          type: 'numeric',
+        });
+
+        ctrl.settings = {
+          rowHeaders: true,
+          colHeaders: true,
+          nestedHeaders: nestedHeaders,
+          columns: columns,
+        };
+
         DesignPoint.$queryAsc().then(function(designPoints) {
           ctrl.groupedDesignPoints = _.groupBy(designPoints, function(designPoint) {
             return designPoint.Member.group;
