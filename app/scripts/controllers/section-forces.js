4ed9f4ffb1c9660193bb4635120df1d0417a0e13
a311cb <tokunaga+a311cb@tokunaga-is.com>
2017-06-20 12:29:53 +0900

    done

--- app/scripts/controllers/section-forces.js
+++ app/scripts/controllers/section-forces.js
@@ -8,8 +8,8 @@
  * Controller of the webdan
  */
 angular.module('webdan')
-  .controller('SectionForcesCtrl', ['$scope', '$filter', 'BendingMoment', 'Shear', 'DesignPoint',
-    function ($scope, $filter, BendingMoment, Shear, DesignPoint) {
+  .controller('SectionForcesCtrl', ['$scope', '$filter', 'BendingMoment', 'Shear', 'DesignPoint', 'hotRegisterer',
+    function ($scope, $filter, BendingMoment, Shear, DesignPoint, hotRegisterer) {
       let ctrl = this;
 
       function init() {
@@ -29,6 +29,34 @@ angular.module('webdan')
           bendingMoments: BendingMoment.settings,
           shears: Shear.settings,
         };
+        angular.forEach(ctrl.settings, function(settings, key) {
+          let hot, lastColIdx, lastRowIdx;
+          settings.enterMoves = function() {
+            hot = hot || hotRegisterer.getInstance(key);
+            lastColIdx = lastColIdx || settings.columns.length - 1;
+            lastRowIdx = lastRowIdx || hot.getSourceData().length - 1;
+            let [startRow, startCol, endRow, endCol] = hot.getSelected();
+
+            let row, col;
+            if (startCol == lastColIdx) {
+              if (startRow == lastRowIdx) {
+                row = -lastRowIdx;
+              }
+              else {
+                row = 1;
+              }
+              col = -(lastColIdx - 3);
+            }
+            else {
+              row = 0;
+              col = 1;
+            }
+            return {
+              row: row,
+              col: col,
+            };
+          };
+        });
       }
 
       init();
