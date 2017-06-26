6e23b5251b8519a3af4198e9c53189eed2faaf3d
a311cb <tokunaga+a311cb@tokunaga-is.com>
2017-06-26 14:21:50 +0900

    done

--- app/scripts/config/app.config.js
+++ app/scripts/config/app.config.js
@@ -18,6 +18,7 @@ angular.module('webdan')
       useDefaults: false,
     },
     CalculationPrint: {
+      sessionStorageKey: 'webdan.postData',
       server: {
         //url: 'http://www.structuralengine.com/RCNonlinear/api/values',
         url: 'https://jsonplaceholder.typicode.com/photos',
--- app/scripts/controllers/calculation-print.js
+++ app/scripts/controllers/calculation-print.js
@@ -8,18 +8,22 @@
  * Controller of the webdan
  */
 angular.module('webdan')
-  .controller('CalculationPrintCtrl', ['$scope', '$filter', '$log', 'CalculationPrint', 'Member', 'calculationPrintConfig', 'RCNonlinear',
-    function ($scope, $filter, $log, CalculationPrint, Member, calculationPrintConfig, RCNonlinear) {
+  .controller('CalculationPrintCtrl', ['$scope', '$window', '$filter', '$log', 'CalculationPrint', 'Member', 'calculationPrintConfig', 'RCNonlinear', 'appConfig',
+    function ($scope, $window, $filter, $log, CalculationPrint, Member, calculationPrintConfig, RCNonlinear, appConfig) {
       let ctrl = this;
 
       ctrl.change = function(key) {
         CalculationPrint.save(ctrl.calculationPrint);
       };
 
-      ctrl.isFormDisabled = function(form) {
-        ctrl.disabled = (form.$invalid || form.$submitted || form.$pristine);
-        return ctrl.disabled;
-      }
+      ctrl.submit = function(form) {
+        let data = CalculationPrint.getStoredData();
+        let json = angular.toJson(data);
+        let key = appConfig.CalculationPrint.sessionStorageKey;
+        sessionStorage.setItem(key, json);
+        $window.open('result.html');
+        form.$setPristine();
+      };
 
       function init() {
         ctrl.config = calculationPrintConfig;
