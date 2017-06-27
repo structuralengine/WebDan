87f64672b8e57ff876177b2563dd251291189cbc
a311cb <tokunaga+a311cb@tokunaga-is.com>
2017-06-28 01:24:27 +0900

    rename ht-helper to ht-utils

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,8 +21,8 @@ angular.module('webdan', [
     'firebase.auth.app',
     'angularMoment',
     'ngHandsontable',
-    'ht-helper',
     'local-db',
+    'ht-utils',
     'ui.bootstrap',
   ])
   .config(['$lowdbProvider', 'dbConfig',
