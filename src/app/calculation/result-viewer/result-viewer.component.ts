750560e5d9b7fbc64089533bc54172d798cb70df
sasaco <sasaicco@gmail.com>
2019-08-24 22:03:39 +0900

    result.html

--- src/app/calculation/result-viewer/result-viewer.component.ts
+++ src/app/calculation/result-viewer/result-viewer.component.ts
@@ -1,33 +1,30 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+﻿import { Component, OnInit } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
-import * as printJS from 'print-js';
+import * as printJS from "print-js";
 // import printJS = require("print-js");
 
-import { SaveDataService } from '../../providers/save-data.service';
-import { WaitDialogComponent } from '../../components/wait-dialog/wait-dialog.component';
+import { SaveDataService } from "../../providers/save-data.service";
+import { WaitDialogComponent } from "../../components/wait-dialog/wait-dialog.component";
 
-import { CalcSafetyMomentService } from '../calc-safety-moment.service';
-import { CalcSafetyShearForceService } from '../calc-safety-shear-force.service';
-import { CalcSafetyFatigueMomentService } from '../calc-safety-fatigue-moment.service';
-import { CalcSafetyFatigueShearForceService } from '../calc-safety-fatigue-shear-force.service';
-import { CalcServiceabilityMomentService } from '../calc-serviceability-moment.service';
-import { CalcServiceabilityShearForceService } from '../calc-serviceability-shear-force.service';
-import { CalcDurabilityMomentService } from '../calc-durability-moment.service';
-import { CalcRestorabilityMomentService } from '../calc-restorability-moment.service';
-import { CalcRestorabilityShearForceService } from '../calc-restorability-shear-force.service';
-import { CalcEarthquakesMomentService } from '../calc-earthquakes-moment.service';
-import { CalcEarthquakesShearForceService } from '../calc-earthquakes-shear-force.service';
-
+import { CalcSafetyMomentService } from "../calc-safety-moment.service";
+import { CalcSafetyShearForceService } from "../calc-safety-shear-force.service";
+import { CalcSafetyFatigueMomentService } from "../calc-safety-fatigue-moment.service";
+import { CalcSafetyFatigueShearForceService } from "../calc-safety-fatigue-shear-force.service";
+import { CalcServiceabilityMomentService } from "../calc-serviceability-moment.service";
+import { CalcServiceabilityShearForceService } from "../calc-serviceability-shear-force.service";
+import { CalcDurabilityMomentService } from "../calc-durability-moment.service";
+import { CalcRestorabilityMomentService } from "../calc-restorability-moment.service";
+import { CalcRestorabilityShearForceService } from "../calc-restorability-shear-force.service";
+import { CalcEarthquakesMomentService } from "../calc-earthquakes-moment.service";
+import { CalcEarthquakesShearForceService } from "../calc-earthquakes-shear-force.service";
 
 @Component({
-  selector: 'app-result-viewer',
-  templateUrl: './result-viewer.component.html',
-  styleUrls: ['./result-viewer.component.scss']
+  selector: "app-result-viewer",
+  templateUrl: "./result-viewer.component.html",
+  styleUrls: ["./result-viewer.component.scss"]
 })
-
 export class ResultViewerComponent implements OnInit {
-
   // 断面力一覧表 /////////////////////////////////////
   // 安全性（破壊）
   private safetyMomentForces: any[];
@@ -69,40 +66,46 @@ export class ResultViewerComponent implements OnInit {
   // 印刷時のスタイル /////////////////////////////////
   private PrintCss: string;
 
-  constructor(private save: SaveDataService,
-              private modalService: NgbModal,
-              private CalcSafetyMoment: CalcSafetyMomentService,
-              private CalcSafetyShearForce: CalcSafetyShearForceService,
-              private CalcSafetyFatigueMoment: CalcSafetyFatigueMomentService,
-              private CalcSafetyFatigueShearForce: CalcSafetyFatigueShearForceService,
-              private CalcServiceabilityMoment: CalcServiceabilityMomentService,
-              private CalcServiceabilityShearForce: CalcServiceabilityShearForceService,
-              private CalcDurabilityMoment: CalcDurabilityMomentService,
-              private CalcRestorabilityMoment: CalcRestorabilityMomentService,
-              private CalcRestorabilityShearForce: CalcRestorabilityShearForceService,
-              private CalcEarthquakesMoment: CalcEarthquakesMomentService,
-              private CalcEarthquakesShearForce: CalcEarthquakesShearForceService) {
+  constructor(
+    private save: SaveDataService,
+    private modalService: NgbModal,
+    private CalcSafetyMoment: CalcSafetyMomentService,
+    private CalcSafetyShearForce: CalcSafetyShearForceService,
+    private CalcSafetyFatigueMoment: CalcSafetyFatigueMomentService,
+    private CalcSafetyFatigueShearForce: CalcSafetyFatigueShearForceService,
+    private CalcServiceabilityMoment: CalcServiceabilityMomentService,
+    private CalcServiceabilityShearForce: CalcServiceabilityShearForceService,
+    private CalcDurabilityMoment: CalcDurabilityMomentService,
+    private CalcRestorabilityMoment: CalcRestorabilityMomentService,
+    private CalcRestorabilityShearForce: CalcRestorabilityShearForceService,
+    private CalcEarthquakesMoment: CalcEarthquakesMomentService,
+    private CalcEarthquakesShearForce: CalcEarthquakesShearForceService
+  ) {
+    this.PrintCss = "@page {";
+    this.PrintCss += "size: A4;";
+    this.PrintCss += "margin: 0;";
+    this.PrintCss += "}";
 
-    this.PrintCss = '@page {';
-    this.PrintCss += 'size: A4;';
-    this.PrintCss += 'margin: 0;';
-    this.PrintCss += '}';
-
-    this.PrintCss += '* {';
-    this.PrintCss += 'font-family:"ＭＳ 明朝", "HG明朝E", "游明朝", YuMincho, "ヒラギノ明朝 ProN W3", "Hiragino Mincho ProN", "ＭＳ Ｐ明朝", serif;';
-    this.PrintCss += 'margin: 0;';
-    this.PrintCss += 'padding: 0;';
-    this.PrintCss += '}';
-
-    this.PrintCss += '.sheet {';
-    this.PrintCss += 'overflow: hidden;';
-    this.PrintCss += 'position: relative;';
-    this.PrintCss += 'box-sizing: border-box;';
-    this.PrintCss += 'page-break-after: always;';
-    this.PrintCss += 'padding-top: 35mm;';
-    this.PrintCss += 'padding-left: 30mm;';
-    this.PrintCss += 'padding-right: 30mm;';
-    this.PrintCss += '}';
+    this.PrintCss += "* {";
+    this.PrintCss +=
+      'font-family:"ＭＳ 明朝", "HG明朝E", "游明朝", YuMincho, "ヒラギノ明朝 ProN W3", "Hiragino Mincho ProN", "ＭＳ Ｐ明朝", serif;';
+    this.PrintCss += "margin: 0;";
+    this.PrintCss += "padding: 0;";
+    this.PrintCss += "}";
+/*
+    this.PrintCss += ".h3 {";
+    this.PrintCss += "  font-size: 11pt;";
+    this.PrintCss += "}";
+*/  
+    this.PrintCss += ".sheet {";
+    this.PrintCss += "overflow: hidden;";
+    this.PrintCss += "position: relative;";
+    this.PrintCss += "box-sizing: border-box;";
+    this.PrintCss += "page-break-after: always;";
+    this.PrintCss += "padding-top: 35mm;";
+    this.PrintCss += "padding-left: 30mm;";
+    this.PrintCss += "padding-right: 30mm;";
+    this.PrintCss += "}";
   }
 
   ngOnInit() {
@@ -127,7 +130,6 @@ export class ResultViewerComponent implements OnInit {
     this.earthquakesMomentForces = this.CalcEarthquakesMoment.setDesignForces();
     this.earthquakesShearForces = this.CalcEarthquakesShearForce.setDesignForces();
 
-
     // 断面照査表 /////////////////////////////////////
     if (this.save.calc.print_selected.print_calculate_checked === true) {
       // 安全性（破壊）
@@ -147,7 +149,6 @@ export class ResultViewerComponent implements OnInit {
       // 復旧性（地震時）
       this.earthquakesMomentPages = this.CalcEarthquakesMoment.getEarthquakesPages();
       this.earthquakesShearForcePages = this.CalcEarthquakesShearForce.getEarthquakesPages();
-
     } else {
       this.safetyMomentPages = new Array();
       this.safetyShearForcePages = new Array();
@@ -163,19 +164,17 @@ export class ResultViewerComponent implements OnInit {
 
       if (this.save.calc.print_selected.print_summary_table_checked === true) {
         // 総括表のみ
-        window.alert('総括表まだ');
+        window.alert("総括表まだ");
       }
     }
     modalRef.close();
-
   }
 
   printTest() {
     printJS({
-      printable: 'print-section',
-      type: 'html',
+      printable: "print-section",
+      type: "html",
       style: this.PrintCss
     });
   }
-
 }
