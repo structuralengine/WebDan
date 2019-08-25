229f67a48cad54a99cff648177fcd0aef4b74212
笹澤陽佑 <sasaicco@gmail.com>
2019-08-26 07:18:00 +0900

    エラーを修正した

--- src/app/calculation/result-data.service.ts
+++ src/app/calculation/result-data.service.ts
@@ -210,7 +210,7 @@ export class ResultDataService {
       return value.g_no === g_no;
     });
     if (safetyList === undefined) {
-      // 安全係数がないので計算対象外
+      console.log("安全係数がないので計算対象外")
       position.SectionData = new Array();
       return;
     }
@@ -238,11 +238,10 @@ export class ResultDataService {
       return (value.m_no === m_no);
     });
     if (memberInfo === undefined) {
+      console.log('部材番号が存在しない');
       position.SectionData = new Array();
-      return; // 部材番号が存在しない
+      return;
     }
-
-    position['memberInfo'] = memberInfo;
 
 
     // POST 用の断面情報をセット
@@ -255,231 +254,223 @@ export class ResultDataService {
           position.SectionData.shift(); // 先頭の要素を取り除く
         }
       }
-      this.getCircle(memberInfo.H, position.SectionData[0]);
-    } else if (memberInfo.shape.indexOf('円形') > 0) {
+      position.SectionData[0]['Sections'] = this.getCircle(memberInfo.H);
+    } else if (memberInfo.shape.indexOf('矩形') > 0) {
 
+    } else if (memberInfo.shape.indexOf('Ｔ形') > 0) {
+      // T形 断面の上側引張は 矩形
+
+    } else if (memberInfo.shape.indexOf('逆Ｔ形') > 0) {
+
+    } else if (memberInfo.shape.indexOf('小判形') > 0) {
+
+    } else if (memberInfo.shape.indexOf('円環') > 0) {
+
+    } else {
+      console.log("断面形状：" + memberInfo.shape + " は適切ではありません。");
+      position.SectionData = new Array();
+      return;
     }
 
-    for (const target of position.SectionData) {
-      const InputData = new Array();
-      switch (memberInfo) {
-      Case "矩形"
-      Set InputData = comon.Init矩形(b, bf, h, hf, Ast, dt, Asc, dc, fck, fsy, Ec, Es, γc, γs, 0)
-      Case "円形"
-      Set InputData = comon.Init円形(b, bf, h, hf, Ast, dt, Asc, dc, fck, fsy, Ec, Es, γc, γs, 0)
-      Case "Ｔ形"
-      Set InputData = comon.InitT形(b, bf, h, hf, Ast, dt, Asc, dc, fck, fsy, Ec, Es, γc, γs, 0)
-      Case "逆Ｔ形"
-      Set InputData = comon.Init逆T(b, bf, h, hf, Ast, dt, Asc, dc, fck, fsy, Ec, Es, γc, γs, 0)
-      Case "小判形"
-      Set InputData = comon.Init小判(b, bf, h, hf, Ast, dt, Asc, dc, fck, fsy, Ec, Es, γc, γs, 0)
-      Case "円環断面"
-      Set InputData = comon.Init円環(b, bf, h, hf, Ast, dt, Asc, dc, fck, fsy, Ec, Es, γc, γs, 0)
-      Case Else
-      MsgBox "断面形状：" & SectType & " は適切ではありません。"
-      Exit Sub
-    }
-    // T形 断面の上側引張は 矩形
-    target['Sections'] = InputData;
+    position['memberInfo'] = memberInfo;
   }
 
 
-}
-// 円形断面の POST 用 データ作成
-private getCircle(h: number, SectionData: any): any[] {
-  const RCOUNT: number = 100;
 
-  const x1: number = h / RCOUNT
-  Dim x2 As Single
-  let b1: number = 0;
-  Dim b2 As Single
-  for ( let i = 0; i < RCOUNT; i++){
-  x2 = x1 * i
-  b2 = this.getCircleWidth(h, x2)
-  Set section = New clsSection
-  With section
-    .Height = x1     '断面高さ
-      .WTop = b1       '断面幅（上辺）
-        .WBottom = b2    '断面幅（底辺）
-          .ElasticID = "c" '材料番号
-  Debug.Print(.Height & "," & .WTop & "," & .WBottom & ",")
-  End With
-  Call InputData.Sections.Add(section)
-  b1 = b2
+  // 円形断面の POST 用 データ作成
+  private getCircle(h: number): any[] {
+    const result: any[] = new Array();
+
+    const RCOUNT: number = 100;
+
+    const x1: number = h / RCOUNT;
+    let b1: number = 0;
+    for (let i = 1; i <= RCOUNT; i++) {
+      const x2: number = x1 * i;
+      const b2: number = this.getCircleWidth(h, x2)
+      const section = {
+        Height: x1,    // 断面高さ 
+        WTop: b1,       //断面幅（上辺）
+        WBottom: b2,    //断面幅（底辺）
+        ElasticID: "c" //材料番号
+      }
+      result.push(section);
+      b1 = b2;
+    }
+    return result;
   }
-}
 
-// 円の頂部からの距離を指定してその円の幅を返す
-private getCircleWidth(R: number, positionFromVertex: number): number {
+  // 円の頂部からの距離を指定してその円の幅を返す
+  private getCircleWidth(R: number, positionFromVertex: number): number {
 
-  const a = R / 2;
-  const x = positionFromVertex;
-  const b = a - x
+    const a = R / 2;
+    const x = positionFromVertex;
+    const b = a - x
 
-  const c = Math.sqrt((a ** 2) - ((a - x) ** 2))
+    const c = Math.sqrt((a ** 2) - ((a - x) ** 2))
 
-  return Math.abs(2 * c);
+    return Math.abs(2 * c);
 
-}
+  }
 
   // position に 鉄筋情報を入力する
   public setBars(g_no: number, m_no: number, position: any): void {
 
-  // 鉄筋配置情報を探す //////////////////////////////////////////////////////
-  const temp = this.save.bars.getBarsColumns();
-  const barList = temp.find(function (value) {
-    return (value[0].g_no === g_no);
-  });
-  if(barList === undefined) {
-  position.SectionData = new Array();
-  return; // 部材グループが存在しない
-}
-
-const startFlg: boolean[] = [false, false];
-let barData: object = null;
-for (let i = barList.length - 1; i >= 0; i--) {
-  // 同じ部材番号を探す
-  if (barList[i].positions.length < 1) { continue; }
-  if (startFlg[0] === false) {
-    if (barList[i].positions[0].m_no === m_no) {
-      startFlg[0] = true;
-    } else {
-      continue;
+    const temp = this.save.bars.getBarsColumns();
+    const barList = temp.find(function (value) {
+      return (value[0].g_no === g_no);
+    });
+    if (barList === undefined) {
+      console.log('部材グループが存在しない')
+      position.SectionData = new Array();
+      return;
     }
-  }
 
-  // 同じ着目点位置を探す
-  for (let j = barList[i].positions.length - 1; j >= 0; j--) {
-    const bar = barList[i].positions[j];
-    if (startFlg[1] === false) {
-      if (bar.index === position.index) {
-        startFlg[1] = true;
-      } else {
-        continue;
+    const startFlg: boolean[] = [false, false];
+    let barData: object = null;
+    for (let i = barList.length - 1; i >= 0; i--) {
+      // 同じ部材番号を探す
+      if (barList[i].positions.length < 1) { continue; }
+      if (startFlg[0] === false) {
+        if (barList[i].positions[0].m_no === m_no) {
+          startFlg[0] = true;
+        } else {
+          continue;
+        }
+      }
+
+      // 同じ着目点位置を探す
+      for (let j = barList[i].positions.length - 1; j >= 0; j--) {
+        const bar = barList[i].positions[j];
+        if (startFlg[1] === false) {
+          if (bar.index === position.index) {
+            startFlg[1] = true;
+          } else {
+            continue;
+          }
+        }
+        // 鉄筋情報を集計
+        if (barData === null) {
+          barData = bar;
+        } else {
+          this.setBarObjectValue(barData, bar);
+        }
       }
     }
-    // 鉄筋情報を集計
-    if (barData === null) {
-      barData = bar;
-    } else {
-      this.setBarObjectValue(barData, bar);
-    }
-  }
-}
 
-position['barData'] = barData;
+    position['barData'] = barData;
   }
 
   // 
   public setPostData(position: any): void {
 
-  // 鉄筋の本数・断面形状を入力する //////////////////////////////////////////////////////
-  for(const target of position.SectionData) {
+    // 鉄筋の本数・断面形状を入力する //////////////////////////////////////////////////////
+    for (const target of position.SectionData) {
 
-  // 断面形状を入力する
-  // memberInfo = {
-  //   'm_no': id, 'm_len': null, 'g_no': null, 'g_name': null, 'shape': '',
-  //   'B': null, 'H': null, 'Bt': null, 't': null,
-  //   'con_u': null, 'con_l': null, 'con_s': null,
-  //   'vis_u': false, 'vis_l': false, 'ecsd': null, 'kr': null,
-  //   'r1_1': null, 'r1_2': null, 'r1_3': null, 'n': null
-  // }
-  const rc = target.safety_factor.M_rc;
-  const fck = target.material_concrete.fck / rc;
-  const Ec = this.getEc(fck);
+      // 断面形状を入力する
+      // memberInfo = {
+      //   'm_no': id, 'm_len': null, 'g_no': null, 'g_name': null, 'shape': '',
+      //   'B': null, 'H': null, 'Bt': null, 't': null,
+      //   'con_u': null, 'con_l': null, 'con_s': null,
+      //   'vis_u': false, 'vis_l': false, 'ecsd': null, 'kr': null,
+      //   'r1_1': null, 'r1_2': null, 'r1_3': null, 'n': null
+      // }
+      const rc = target.safety_factor.M_rc;
+      const fck = target.material_concrete.fck / rc;
+      const Ec = this.getEc(fck);
 
-  const FrameWebPostData = {
-    Md: Math.abs(target.Md),
-    Nd: target.Nd,
-    Sections: [],
-    SectionElastic: [{
-      fck: fck,
-      Ec: Ec,
-      ElasticID: 'C'
-    }],
-    Steels: [{
+      const FrameWebPostData = {
+        Md: Math.abs(target.Md),
+        Nd: target.Nd,
+        Sections: [],
+        SectionElastic: [{
+          fck: fck,
+          Ec: Ec,
+          ElasticID: 'C'
+        }],
+        Steels: [{
 
-    }],
-    SteelElastic: [{
-      fsk: 345,
-      Es: 200,
-      ElasticID: 'S'
-    }]
-  };
-  // 鉄筋の本数を入力する
-  switch (target.memo) {
-    case '上側引張':
-      break;
-    case '下側引張':
-      break;
-  }
+        }],
+        SteelElastic: [{
+          fsk: 345,
+          Es: 200,
+          ElasticID: 'S'
+        }]
+      };
+      // 鉄筋の本数を入力する
+      switch (target.memo) {
+        case '上側引張':
+          break;
+        case '下側引張':
+          break;
+      }
 
-}
+    }
   }
   // 連想配列の null の要素をコピーする
   private setBarObjectValue(target: object, obj: object): void {
-  try {
-    for(const key of Object.keys(obj)) {
-  if (obj[key] === undefined) { continue; }
-  if (obj[key] === null) { continue; }
-  if (key === 'haunch_M') { continue; }
-  if (key === 'haunch_V') { continue; }
-  if (key === 'tan') { continue; }
-  if (key === 'b') { continue; }
-  if (key === 'h') { continue; }
-  if (key === 'm_no') { continue; }
-  if (key === 'p_name') { continue; }
-  if (key === 'p_name_ex') { continue; }
-  if (key === 'position') { continue; }
-  if (key === 'index') { continue; }
-  if (key === 'title') { continue; }
-  if (key === 'cos') { continue; }
-  if (key === 'enable') { continue; }
+    try {
+      for (const key of Object.keys(obj)) {
+        if (obj[key] === undefined) { continue; }
+        if (obj[key] === null) { continue; }
+        if (key === 'haunch_M') { continue; }
+        if (key === 'haunch_V') { continue; }
+        if (key === 'tan') { continue; }
+        if (key === 'b') { continue; }
+        if (key === 'h') { continue; }
+        if (key === 'm_no') { continue; }
+        if (key === 'p_name') { continue; }
+        if (key === 'p_name_ex') { continue; }
+        if (key === 'position') { continue; }
+        if (key === 'index') { continue; }
+        if (key === 'title') { continue; }
+        if (key === 'cos') { continue; }
+        if (key === 'enable') { continue; }
 
-  if (typeof obj[key] === 'object') {
-    this.setBarObjectValue(target[key], obj[key]);
-  } else {
-    if (target[key] === null) {
-      target[key] = obj[key];
-    }
-  }
-}
+        if (typeof obj[key] === 'object') {
+          this.setBarObjectValue(target[key], obj[key]);
+        } else {
+          if (target[key] === null) {
+            target[key] = obj[key];
+          }
+        }
+      }
     } catch {
-  console.log('aa');
-}
+      console.log('aa');
+    }
   }
 
   private getEc(fck: number) {
 
-  const EcList: number[] = [22, 25, 28, 31, 33, 35, 37, 38];
-  const fckList: number[] = [18, 24, 30, 40, 50, 60, 70, 80];
+    const EcList: number[] = [22, 25, 28, 31, 33, 35, 37, 38];
+    const fckList: number[] = [18, 24, 30, 40, 50, 60, 70, 80];
 
-  let i: number;
-  const j: number = fckList.length - 1;
+    let i: number;
+    const j: number = fckList.length - 1;
 
-  let x1: number;
-  let x2: number;
-  let y1: number;
-  let y2: number;
-  if (fckList[0] >= fck) {
-    x1 = fckList[0];
-    x2 = fckList[1];
-    y1 = EcList[0];
-    y2 = EcList[1];
-  } else if (fckList[j] <= fck) {
-    i = j;
-  } else {
-    for (i = 0; i < fckList.length; i++) {
-      if (fckList[i] >= fck) {
-        break;
+    let x1: number;
+    let x2: number;
+    let y1: number;
+    let y2: number;
+    if (fckList[0] >= fck) {
+      x1 = fckList[0];
+      x2 = fckList[1];
+      y1 = EcList[0];
+      y2 = EcList[1];
+    } else if (fckList[j] <= fck) {
+      i = j;
+    } else {
+      for (i = 0; i < fckList.length; i++) {
+        if (fckList[i] >= fck) {
+          break;
+        }
       }
+      x1 = fckList[i - 1];
+      x2 = fckList[i];
+      y1 = EcList[i - 1];
+      y2 = EcList[i];
     }
-    x1 = fckList[i - 1];
-    x2 = fckList[i];
-    y1 = EcList[i - 1];
-    y2 = EcList[i];
+    return y2 + (x2 - fck) * (y1 - y2) / (x2 - x1);
   }
-  return y2 + (x2 - fck) * (y1 - y2) / (x2 - x1);
-}
 
 }
