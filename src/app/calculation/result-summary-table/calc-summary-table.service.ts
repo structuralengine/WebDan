bbd1023e0ad133138a9f2ab58ba8ff0ab53f582a
sasaco <sasaicco@gmail.com>
2021-05-29 16:35:51 +0900

    エクセルファイルを生成しダウンロードできるようにした

--- src/app/calculation/result-summary-table/calc-summary-table.service.ts
+++ src/app/calculation/result-summary-table/calc-summary-table.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { utils, write, WorkBook } from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
@@ -63,7 +65,8 @@ export class CalcSummaryTableService {
 
     // 全ての項目が終了したかチェックする
     if(this.checkDone()){
-      alert("計算が終了しました")
+      alert("計算が終了しました");
+      this.initttt();
     }
 
   }
@@ -90,5 +93,43 @@ export class CalcSummaryTableService {
   }
 
 
+
+  title = 'app';
+  table = [
+    {
+      First: 'one',
+      Second: 'two',
+      Third: 'three',
+      Forth: 'four',
+      Fifth: 'five'
+    },
+    {
+      First: 'un',
+      Second: 'deux',
+      Third: 'trois',
+      Forth: 'quatre',
+      Fifth: 'cinq'
+    },
+  ];
+
+  initttt() {
+    const ws_name = 'SomeSheet'; // シート名
+    const wb: WorkBook = { SheetNames: [], Sheets: {} };
+    const ws: any = utils.json_to_sheet(this.table);
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+    const wbout = write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+
+    function s2ab(s) {
+      const buf = new ArrayBuffer(s.length);
+      const view = new Uint8Array(buf);
+      for (let i = 0; i !== s.length; ++i) {
+        view[i] = s.charCodeAt(i) & 0xFF;
+      };
+      return buf;
+    }
+
+    saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }), 'exported.xlsx');
+  }
 
 }
